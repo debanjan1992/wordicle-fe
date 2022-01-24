a11d4d94a5a9e62c41e381c5b97608bfd907efe5
Debanjan Saha <debanjansaha1992@gmail.com>
2022-01-24 15:04:09 +0530

    service worker changes

---
+++ src/sw.js
@@ -0,0 +1,27 @@
+var CACHE_NAME = "wordicle-cache-v1";
+var urlsToCache = ["/", "/assets/", "/index.html", "/main.bundle.js"];
+
+self.addEventListener("install", function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    (async function () {
+      try {
+        var res = await fetch(event.request);
+        var cache = await caches.open("cache");
+        cache.put(event.request.url, res.clone());
+        return res;
+      } catch (error) {
+        return caches.match(event.request);
+      }
+    })()
+  );
+});
