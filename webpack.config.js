460f26c26ac052a441355be644a27056dc23bdf2
Debanjan Saha <debanjansaha1992@gmail.com>
2022-01-24 11:24:32 +0530

    changed webpack to copy assets

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 const Dotenv = require('dotenv-webpack');
 
 const config = (env) => {
@@ -38,7 +39,12 @@ const config = (env) => {
             new HtmlWebpackPlugin({
                 template: path.join(__dirname, "src", "index.html"),
             }),
-            new MiniCssExtractPlugin()
+            new MiniCssExtractPlugin(),
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: "src/assets", to: "assets" }
+                ]
+            })
         ]
     };
 
